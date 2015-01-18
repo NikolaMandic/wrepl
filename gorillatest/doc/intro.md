52682ab4afb76a4beba2952332a88a0710e9ec13
NikolaMandic <mn080202@gmail.com>
2015-01-18 02:28:13 +0100

    first commit

---
+++ gorillatest/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to gorillatest
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ gorillatest/test/gorillatest/core_test.clj
@@ -0,0 +1,7 @@
+(ns gorillatest.core-test
+  (:require [clojure.test :refer :all]
+            [gorillatest.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
